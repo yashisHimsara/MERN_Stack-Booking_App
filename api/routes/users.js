ef6a94cbe81f3c9f58ee4f3a092aeb0c8a3814dc
yashisHimsara <139766123+yashisHimsara@users.noreply.github.com>
2025-03-23 21:58:38 +0530

    Update users

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,52 @@
 import express from "express";
+import User from "../models/User.js";
 
 const router = express.Router();
 
-export default router
+// Create a new user
+router.post("/", async (req, res) => {
+    const newUser = new User(req.body);
+
+    try {
+        const savedUser = await newUser.save();
+        res.status(200).json(savedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Update user by ID
+router.put("/:id", async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true }
+        );
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Get user by ID
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Get all users
+router.get("/", async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+export default router;
